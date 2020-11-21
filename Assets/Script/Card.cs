9bbb860e2487c789e31b9fb537ae8da7da203d1d
LeandroCDN <63496697+LeandroCDN@users.noreply.github.com>
2020-11-21 16:24:54 -0300

    Primer commit
    Tiene Desarrollado
     Prefab de carta basico (TODO, logica matematica)
     Movimiento de carta
     Presentacion de escenario.

---
+++ Assets/Script/Card.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Card : MonoBehaviour
+{
+    public int id; //lugar que ocupara la carta y forma para identificarla
+    public int value; //Sera el valor de la Carta   
+    private Transform target; // Posicion que buscara segun su id
+    public float speed = 0.5f; //velocidad para llegar a su posicion
+    public bool esta = false; // si la carta esta o no en la mesa
+    public Text valueText; // canvas del valor de la carta
+
+    private void Start()
+    {
+        target = PositionsCards.posId[id];        
+        value = (int)((Random.Range(1, 50)) + 0.5);
+    }
+
+    private void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+        transform.rotation = target.rotation;
+        valueText.text = "" + value;
+    }
+
+    public Card (int Id, int Value)
+    {
+        id = Id;
+        value = Value;
+    }
+}
---
+++ Assets/Script/PositionsCards.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionsCards : MonoBehaviour
+{
+    public static Transform[] posId;
+
+    private void Awake()
+    {
+        posId = new Transform[transform.childCount];
+        for (int  i = 0; i < posId.Length; i++)
+        {
+           posId[i] = transform.GetChild(i);
+        }
+    }
+}
